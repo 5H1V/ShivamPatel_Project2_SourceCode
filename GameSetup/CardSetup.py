5c58b5a516726e45a4690ce8415c066d13286a9e
5H1V <spatel0240@gmail.com>
2025-07-24 21:17:14 -0400

    Added board matrix to track players. Added visualization of board matrix in graph form. Updated card dealings based on indices from board matrix

--- GameSetup/CardSetup.py
+++ GameSetup/CardSetup.py
@@ -13,46 +13,47 @@ class CardSetup:
                      "peacock" : "Mrs. Peacock",
                      "plum" : "Professor Plum"}
 
-    players = {0 : "Miss Scarlett",
-               1 : "Colonel Mustard",
-               2 : "Mrs. White",
-               3 : "Reverend Green",
-               4 : "Mrs. Peacock",
-               5 : "Professor Plum"}
+    rooms = {2 : "Ballroom",
+             3 : "BillardRoom",
+             4 : "Conservatory",
+             5 : "DiningRoom",
+             6 : "Hall",
+             7 : "Kitchen",
+             8 : "Library",
+             9 : "Lounge",
+             10 : "Study"}
 
-    weapons = {6 : "Candlestick",
-               7 : "Dagger",
-               8 : "Lead Pipe",
-               9 : "Revolver",
-               10 : "Rope",
-               11 : "Wrench"}
+    players = {11 : "Miss Scarlett",
+               12 : "Colonel Mustard",
+               13 : "Mrs. White",
+               14 : "Reverend Green",
+               15 : "Mrs. Peacock",
+               16 : "Professor Plum"}
 
-    rooms = {12 : "Ballroom",
-             13 : "BillardRoom",
-             14 : "Conservatory",
-             15 : "DiningRoom",
-             16 : "Hall",
-             17 : "Kitchen",
-             18 : "Library",
-             19 : "Lounge",
-             20 : "Study"}
+    weapons = {17 : "Candlestick",
+               18 : "Dagger",
+               19 : "Lead Pipe",
+               20 : "Revolver",
+               21 : "Rope",
+               22 : "Wrench"}
 
     def solution(players, weapons, rooms):
-        player_sol = np.random.randint(0,6)
-        weapon_sol = np.random.randint(6,12)
-        room_sol = np.random.randint(12,21)
-    
+        room_sol = np.random.randint(2,11)
+        player_sol = np.random.randint(11,17)
+        weapon_sol = np.random.randint(17,23)
+
         return {player_sol : players[player_sol],
                 weapon_sol : weapons[weapon_sol],
                 room_sol : rooms[room_sol]}
 
     def hand_out_cards(players, weapons, rooms, solution):
-        remaining_cards = [card for card in range(21) if card not in solution]
+        remaining_cards = [card for card in range(2,23) if card not in solution]
         random.shuffle(remaining_cards)
-        num_cards = len(remaining_cards)/len(players)
         card_hands = {player: [] for player in players}
-        for player, hand in card_hands.items():
-            hand[player:player+3] = remaining_cards[player*3:player*3+3]
+        for i in range(3):
+            for player in players:
+                if remaining_cards:
+                    card_hands[player].append(remaining_cards.pop())
 
         return card_hands
 
--- GameSetup/DrawBoard.py
+++ GameSetup/DrawBoard.py
@@ -1,6 +1,7 @@
+import numpy as np
+from matplotlib.path import Path
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-import numpy as np
 
 
 
@@ -10,85 +11,97 @@ class DrawBoard:
 
         return
 
-    def draw_cluedo_board():
-        fig, ax = plt.subplots()
+    board_labels = {"Invalid": -1,
+                    "Empty" : 0,
+                    "Occupied" : 1,
+                    "Ballroom" : 2,
+                    "BillardRoom" : 3,
+                    "Conservatory" : 4,
+                    "DiningRoom" : 5,
+                    "Hall" : 6,
+                    "Kitchen" : 7,
+                    "Library" : 8,
+                    "Lounge" : 9,
+                    "Study" : 10,
+                    "Center" : 11,
+                    "Door" : 12,
+                    "Secret" : 13}
 
-        # Coordinates of room boundaries
-        rooms = {
-            "Lounge": [(0,25),(6,25),(6,24),(7,24),(7,19),(0,19)],
-            "Hall": [(9,25),(15,25),(15,18),(9,18)],
-            "Study": [(18,25),(24,25),(24,21),(17,21),(17,24),(18,24)],
-            "DiningRoom": [(0,16),(8,16),(8,10),(5,10),(5,9),(0,9)],
-            "Library": [(18,19),(23,19),(23,18),(24,18),(24,15),(23,15),(23,14),(18,14),(18,15),(17,15),(17,18),(18,18)],
-            "BillardRoom": [(18,13),(24,13),(24,8),(18,8)],
-            "Kitchen": [(1,7),(6,7),(6,1),(0,1),(0,6),(1,6)],
-            "Ballroom": [(8,8),(16,8),(16,2),(14,2),(14,0),(10,0),(10,2),(8,2)],
-            "Conservatory": [(19,6),(23,6),(23,5),(24,5),(24,1),(18,1),(18,5),(19,5)],
-            "Center": [(10,17),(15,17),(15,10),(10,10)],
-        }
+    rooms = {
+        "Lounge": [(0,25),(6,25),(6,24),(7,24),(7,19),(0,19)],
+        "Hall": [(9,25),(15,25),(15,18),(9,18)],
+        "Study": [(18,25),(24,25),(24,21),(17,21),(17,24),(18,24)],
+        "DiningRoom": [(0,16),(8,16),(8,10),(5,10),(5,9),(0,9)],
+        "Library": [(18,19),(23,19),(23,18),(24,18),(24,15),(23,15),(23,14),(18,14),(18,15),(17,15),(17,18),(18,18)],
+        "BillardRoom": [(18,13),(24,13),(24,8),(18,8)],
+        "Kitchen": [(1,7),(6,7),(6,1),(0,1),(0,6),(1,6)],
+        "Ballroom": [(8,8),(16,8),(16,2),(14,2),(14,0),(10,0),(10,2),(8,2)],
+        "Conservatory": [(19,6),(23,6),(23,5),(24,5),(24,1),(18,1),(18,5),(19,5)],
+        "Center": [(10,17),(15,17),(15,10),(10,10)],
+    }
 
-
-        # Doorway positions top left (x, y)
-        doors = [(6,19),(11,18),(12,18),(14,20),(17,21),(17,16),(20,14),(22,12),(18,9),(18,4),
-                 (6,15),(7,12),(4,6),(8,5),(9,7),(14,7),(15,5)]
-
-        # Draw rooms
+    def position_matrix(board_labels, rooms):
+        board = np.zeros((25,24))
         for name, coordinates in rooms.items():
-            x, y = zip(*coordinates)
-            plt.fill(x, y, color='lightgray')
-            plt.plot(x + (x[0],), y + (y[0],), color='r')
+            poly_path = Path(coordinates)
+            for y in range(25):
+                for x in range(25):
+                    if poly_path.contains_point((x + 0.5, y + 0.5)):
+                        board[y, x] = board_labels[name]
 
 
+        doorway = [(6,19),(11,18),(12,18),(14,20),(17,21),(17,16),
+                (20,14),(6,15),(7,12),(22,12),(18,9),(4,6),
+                (8,5),(9,7),(14,7),(15,5),(18,4)]
 
-        # Draw doorways - 17
-        doorway = [(6,7,19,19),(11,13,18,18),(15,15,21,20),(17,18,21,21),(17,17,17,16),
-                (20,21,14,14),(6,7,16,16),(8,8,13,12),(22,23,13,13),(18,18,10,9),
-                (4,5,7,7),(8,8,6,5),(9,10,8,8),(14,15,8,8),(16,16,6,5),(18,19,5,5)]
-        
-        # Draw each remaining edge individually
-        for x1,x2,y1,y2 in doorway:
-            plt.plot([x1,x2],[y1,y2],color='blue')
+        for x,y in doorway:
+            board[y, x] = board_labels["Door"]
 
-        secret = [(6,7,20,20),(17,18,22,22),(4,5,6,6),(18,19,4,4)]
+        starting_positions = [(9,0),(14,0),(23,6),(23,19),(7,24),(0,17)]
+        for x, y in starting_positions:
+            board[y, x] = board_labels["Occupied"]
 
-        for x1,x2,y1,y2 in secret:
-            plt.plot([x1,x2],[y1,y2],color='green')
+        secret = [(4,5),(18,3),(17,22),(6,20)]
+        for x, y in secret:
+            board[y, x] = board_labels["Secret"]
 
-        game_walls = [(0,25),(6,25),(6,24),(7,24),(7,25),(8,25),(8,24),(9,24),(9,25),
-                    (15,25),(15,24),(16,24),(16,25),(17,25),(17,24),(18,24),(18,25),
-                    (24,25),(24,21),(23,21),(23,20),(24,20),(24,19),(23,19),(23,18),
-                    (24,18),(24,15),(23,15),(23,13),(24,13),(24,8),(23,8),(23,7),(24,7),
-                    (24,6),(23,6),(23,5),(24,5),(24,1),(18,1),(18,2),(17,2),(17,1),
-                    (15,1),(15,0),(9,0),(9,1),(7,1),(7,2),(6,2),(6,1),(0,1),(0,6),(1,6),
-                    (1,7),(0,7),(0,8),(1,8),(1,9),(0,9),(0,16),(1,16),(1,17),(0,17),
-                    (0,18),(1,18),(1,19),(0,19),(0,25)]
-        
-        x_vals = [x for x,y in game_walls]
-        y_vals = [y for x,y in game_walls]
-        
-        plt.plot(x_vals, y_vals, color='black')
+        game_walls = {0:[0,1,2,3,4,5,6,7,8,15,16,17,18,19,20,21,22,23],
+                    1:[6,17], 5:[23], 6:[0], 7:[23], 8:[0], 13:[23],
+                    14:[23], 16:[0], 18:[0,23], 20:[23], 24:[6,8,15,17]}
 
-        starting_positions = [(7,24),(23,19),(23,6),(14,0),(9,0),(0,17)]
+        for row, cols in game_walls.items():
+            for col in cols:
+                board[row, col] = board_labels["Invalid"]
 
-        for x,y in starting_positions:
-            position = patches.Rectangle((x, y), 1, 1, facecolor='lightgrey')
-            ax.add_patch(position)
-        
-        plt.xticks([])
-        plt.yticks([])
-        for spine in plt.gca().spines.values():
-            spine.set_visible(False)
+        np.set_printoptions(linewidth=200)
+        print(board.astype(int))
 
-        plt.gca().invert_yaxis()
-        plt.gca().set_aspect('equal', adjustable='box')
+        return board
+
+    def drawBoard(board):
+        cmap = {-1 : "black", 0 : "white", 1 :"skyblue", 11 : "grey", 12 : "coral", 13 : "lightgreen"}
+        fig, ax = plt.subplots()
+        for y in range(25):
+            for x in range(24):
+                color = cmap.get(board[y, x], "gold")
+                ax.add_patch(plt.Rectangle((x, 24 - y), 1, 1, color=color))
+        ax.set_xlim(0, 24)
+        ax.set_ylim(0, 25)
+        ax.set_xticks(range(25))
+        ax.set_yticks(range(26))
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.grid(True)
+        ax.set_aspect('equal')
 
         plt.show()
 
 
 
-
 if __name__=="__main__":
-    DrawBoard.draw_cluedo_board()
+    position_matrix = DrawBoard.position_matrix(DrawBoard.board_labels,DrawBoard.rooms)
+    
+    DrawBoard.drawBoard(position_matrix)
 """
 Mansion Layout:
 
